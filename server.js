3032f292c5cbc32a5644475e67c4c762efe69c08
Tucker Margulies <marguliw@gmail.com>
2016-02-19 18:39:01 -0500

    php trick

---
+++ server.js
@@ -0,0 +1,17 @@
+// var express      = require('express'),
+//     server       = express(),
+//     bodyParser   = require('body-parser');
+//     PORT         = process.env.PORT || 3000.
+//
+//
+//   server.use(express.static('./public'));
+//
+//
+//   server.listen(PORT, function(){
+//     console.log("Server is listening on " + PORT);
+//   });
+//
+//
+// server.get('/', function (req, res){
+//   res.render('index')
+// })
