15058825a3b8ba1083e609f5d3f61abb18d20865
Tucker Margulies <marguliw@gmail.com>
2016-02-19 19:11:52 -0500

    node server fixes

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-// var express      = require('express'),
-//     server       = express(),
-//     bodyParser   = require('body-parser');
-//     PORT         = process.env.PORT || 3000.
-//
-//
-//   server.use(express.static('./public'));
-//
-//
-//   server.listen(PORT, function(){
-//     console.log("Server is listening on " + PORT);
-//   });
-//
-//
-// server.get('/', function (req, res){
-//   res.render('index')
-// })
+var express      = require('express'),
+    server       = express(),
+    bodyParser   = require('body-parser');
+    PORT         = process.env.PORT || 3000.
+
+
+  server.use(express.static('./public'));
+
+  server.listen(PORT, function(){
+    console.log("Server is listening on " + PORT);
+  });
+
+
+server.get('/', function (req, res){
+  res.render('index')
+})
